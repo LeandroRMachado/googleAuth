a0dae9cbf4571431800d61392e5b21a0da19898a
LeandroRMachado <leandroskullofwar@outlook.com>
2022-08-02 12:12:46 -0300

    styled screen SignIn

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '../src/styles/global.scss';
+
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
